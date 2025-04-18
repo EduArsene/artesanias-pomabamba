199afaab48dc7fafee983b3d5e069785da3e1e16
Eduardo Vergaray <eduardoanthonny283@gmail.com>
2025-04-17 20:02:33 -0500

    Este mensaje es para confirmar que salio bien todo :,)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/artesanias-pomabamba/', // usa el nombre exacto de tu repo
   plugins: [react()],
 })
